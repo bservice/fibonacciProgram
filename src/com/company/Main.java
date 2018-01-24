9c54c2dab4860b8584cddaa89981766f7a2505bf
bs666 <brservice@comcast.net>
2018-01-24 08:50:21 -0500

    it works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -32,14 +32,11 @@ public class Main {
     }
 
     public static int fibonacci(int n) {
-        if (n == 0)
-        {
-            return 0;
-        }
-        else if (n==1)
+        if (n <= 1)
         {
             return n;
         }
+
         else
         {
             return fibonacci(n-1) + fibonacci(n-2);
